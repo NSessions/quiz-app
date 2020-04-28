e76ed04fdeb5ee6a60b6391a3699d928329535c3
Nathan Sessions <nathan.m.sessions@gmail.com>
2020-04-28 14:51:44 -0600

    Wireframed the pages.

---
+++ store.js
@@ -0,0 +1,57 @@
+const STORE = {
+    questions: [
+        {
+            question: "In our solar system, what place is Earth in terms of distance from the Sun?",
+            options:[
+                "Fourth",
+                "Sixth",
+                "Eighth",
+                "Third"
+            ],
+            correctAnswer: "Third"
+        },
+        {
+            question: "",
+            options:[
+                "",
+                "",
+                "",
+                ""
+            ],
+            correctAnswer: ""
+        },
+        {
+            question: "",
+            options:[
+                "",
+                "",
+                "",
+                ""
+            ],
+            correctAnswer: ""
+        },
+        {
+            question: "",
+            options:[
+                "",
+                "",
+                "",
+                ""
+            ],
+            correctAnswer: ""
+        },
+        {
+            question: "",
+            options:[
+                "",
+                "",
+                "",
+                ""
+            ],
+            correctAnswer: ""
+        }
+    ],
+    currentQuestion: 0,
+    currentScore: 0
+
+};
