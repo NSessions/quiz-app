6a147be2ac6bdeed343e426eccadb3db2efb3e37
Nathan Sessions <nathan.m.sessions@gmail.com>
2020-04-29 01:58:29 -0600

    Added all the pages and added answer options for questions.

--- store.js
+++ store.js
@@ -13,40 +13,40 @@ const STORE = {
         {
             question: "How old is the universe in years?",
             options:[
-                "",
-                "",
-                "",
-                ""
+                "2020",
+                "13.8 million",
+                "123,405",
+                "13.8 billion"
             ],
-            correctAnswer: "13.8 billion years old"
+            correctAnswer: "13.8 billion"
         },
         {
             question: "What has a gravitational pull so strong that even light cannot escape it?",
             options:[
-                "",
-                "",
-                "",
-                ""
+                "The Sun",
+                "A black hole",
+                "A neutron star",
+                "Dark matter"
             ],
             correctAnswer: "A black hole"
         },
         {
             question: "What is the smallest planet in our solar system?",
             options:[
-                "",
-                "",
-                "",
-                ""
+                "Earth",
+                "Pluto",
+                "Mercury",
+                "Uranus"
             ],
             correctAnswer: "Mercury"
         },
         {
             question: "What is the largest type of star in the universe?",
             options:[
-                "",
-                "",
-                "",
-                ""
+                "Red dwarf stars",
+                "Protostars",
+                "Neutron stars",
+                "Red supergiant stars"
             ],
             correctAnswer: "Red supergiant stars"
         }
